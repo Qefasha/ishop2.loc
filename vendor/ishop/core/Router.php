f79da42d56064f62b4e469136fc0ce40ce6c89c9
Qefasha <ogri96@gmail.com>
2020-09-28 18:08:26 +0700

    Настройка маршрутизатора

--- vendor/ishop/core/Router.php
+++ vendor/ishop/core/Router.php
@@ -20,19 +20,67 @@ class Router
         return self::$routes;
     }
 
-    public static function getRoute(){
+    public static function getRoute()
+    {
         return self::$route;
     }
 
-    public static function dispatch($url){             // вызывает matchRoute($url) (вернет либо есть, либо нет) Либо вызывает соотв контроллер или вызывает ошибку 404
-        if(self::matchRoute($url)) {                   // создаем условие для matchRoute
-            echo 'OK';
-        }else{
-            echo 'NO';
+    public static function dispatch($url)
+    {             // вызывает matchRoute($url) (вернет либо есть, либо нет) Либо вызывает соотв контроллер или вызывает ошибку 404
+        if (self::matchRoute($url)) {                   // создаем условие для matchRoute
+            $controller = 'app\controllers\\' . self::$route['prefix'] . self::$route['controller'] . 'Controller';
+            if (class_exists($controller)){
+                $controllerObject = new $controller(self::$route);
+                $action = self::lowerCamelCase (self::$route['action']) . 'Action';
+                if (method_exists($controllerObject, $action)){
+                    $controllerObject->$action();
+                } else {
+                    throw new \Exception("Метод $controller::$action не найдена", 404);
+                }
+            } else {
+            throw new \Exception("Контроллер $controller не найдена", 404);
+        }
+        } else {
+            throw new \Exception("Страница не найдена", 404);
         }
     }
 
-    public static function matchRoute($url) {          // ищет соответстиве в таблице маршрутов
+    public static function matchRoute($url)
+    {          // ищет соответстиве в таблице маршрутов
+        foreach (self::$routes as $pattern => $route) {
+            if (preg_match("#{$pattern}#", $url, $matches)) {
+                foreach ($matches as $k => $v) {
+                    if (is_string($k)) {
+                        $route[$k] = $v;
+                    }
+                }
+                if (empty($route['action'])) {
+                    $route['action'] = 'index';
+                }
+                if (!isset($route['prefix'])) {
+                    $route['prefix'] = '';
+                } else {
+                    $route['prefix'] .= '\\';
+                }
+                $route['contrpller'] = self::upperCamelCase($route['controller']);
+                self::$route = $route;
+                debug(self::$route);
+                return true;
+            }
+        }
+        return false;
     }
 
+    //CamelCase
+    protected static function upperCamelCase($name){
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $name)));
+    }
+
+    // camelCase
+    protected static function lowerCamelCase($name){
+        return lcfirst(self::upperCamelCase($name));
+
+    }
 }
+
+
